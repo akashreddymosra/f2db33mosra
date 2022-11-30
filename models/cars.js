638b0777feb5983be34d6c731ce8f39e877b1471
akashreddymosra <akashreddymosra@gmail.com>
2022-11-30 16:41:40 -0600

    commited

--- models/cars.js
+++ models/cars.js
@@ -1,8 +1,34 @@
-const mongoose = require("mongoose") 
-const carsSchema = mongoose.Schema({ 
- carBrand: String, 
- carcolor: String, 
- carcost: Number 
-}) 
- 
+const mongoose = require("mongoose")
+const carsSchema = mongoose.Schema({
+    carBrand: {
+
+        type: String,
+
+        min: 1,
+
+        max: 100
+
+    },
+
+    carcolor: {
+
+        type: String,
+
+        min: 1,
+
+        max: 100
+
+    },
+
+    carcost: {
+
+        type: Number,
+
+        min: 1,
+
+        max: 10000000
+
+    }
+})
+
 module.exports = mongoose.model("cars", carsSchema)
--- routes/cars.js
+++ routes/cars.js
@@ -1,5 +1,14 @@
 var express = require('express');
 const cars_controlers= require('../controllers/cars'); 
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
 var router = express.Router();
 
 /* GET costumes */
@@ -9,10 +18,10 @@ router.get('/', cars_controlers.cars_view_all_Page );
 /* GET detail cars page */
 router.get('/detail', cars_controlers.cars_view_one_Page);
 /* GET create cars page */
-router.get('/create', cars_controlers.cars_create_Page);
+router.get('/create',secured, cars_controlers.cars_create_Page);
 /* GET create update page */
-router.get('/update', cars_controlers.cars_update_Page);
+router.get('/update',secured, cars_controlers.cars_update_Page);
 /* GET delete cars page */
-router.get('/delete', cars_controlers.cars_delete_Page);
+router.get('/delete',secured, cars_controlers.cars_delete_Page);
 
 module.exports = router;
